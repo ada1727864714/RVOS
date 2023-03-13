90136cd908ef76e745773e8b1fd161c5820f4704
ada1727864714 <1727864714@qq.com>
2023-03-13 17:07:46 +0800

    entry.S加入trap处理函数

---
+++ Code/include/riscv.h
@@ -0,0 +1,16 @@
+/*
+ * @Author: error: error: git config user.name & please set dead value or install git && error: git config user.email & please set dead value or install git & please set dead value or install git
+ * @Date: 2023-03-13 16:53:55
+ * @LastEditors: error: error: git config user.name & please set dead value or install git && error: git config user.email & please set dead value or install git & please set dead value or install git
+ * @LastEditTime: 2023-03-13 17:00:46
+ * @FilePath: /RVOS/Code/include/riscv.h
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+#ifndef __RISCV_H__
+#define __RISCV_H__
+
+#include "types.h"
+
+
+
+#endif /* __RISCV_H__ */
